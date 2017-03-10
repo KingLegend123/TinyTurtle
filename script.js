29250b64b46d8ac02f913509b0c7ba25a2a4155c
KingLegend123 <mastershane21@gmail.com>
2017-03-10 14:40:05 +0000

    my comment

---
+++ script.js
@@ -0,0 +1,15 @@
+ TinyTurtle.apply(window); 
+forward(40);
+right(90);
+forward(40)
+right(90)
+forward(40)
+right(90)
+forward(40)
+right(90)
+forward(40)
+right(30)
+forward(40)
+right(120)
+forward(40)
+stamp()
